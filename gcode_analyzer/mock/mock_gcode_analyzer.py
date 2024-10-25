4c05629f08f28e9211c3dfcc8b23345003dc2222
Shohei Aoki <shoaok@gmail.com>
2024-10-25 08:52:42 +0300

    refactored

--- gcode_analyzer/mock/mock_gcode_analyzer.py
+++ gcode_analyzer/mock/mock_gcode_analyzer.py
@@ -14,6 +14,11 @@ def inverse_kinematics_dummy(X,Y,Z):
 def write_to_KVP_memory_dummy(A1,A2,A3,A4,A5,A6):
     pass
 
+def dump_value(X,Y,Z,A1,A2,A3,A4,A5,A6):
+    print('X:'+str(X)+', Y:'+str(Y)+', Z:'+str(Z),end='')
+    print('-> A1:'+str(A1)+', A2:'+str(A2)+', A3: '+str(A3)+', A4:'+str(A4)+', A5:'+str(A5)+', A6:'+str(A6))
+    print()
+
 filename = 'test.gcode'
 fin = open(filename)
 X = 0
@@ -24,20 +29,17 @@ for line in fin.readlines():
     if line.startswith('G1'):
         if 'X' in line:
             X = line.split('X')[1].split(' ')[0]
-            print('X:'+str(X)+' ',end='')
         if 'Y' in line:
             Y = line.split('Y')[1].split(' ')[0]
-            print('Y:'+str(Y)+' ',end='')
         if 'Z' in line:
             Z = line.split('Z')[1].split(' ')[0]
-            print('Z:'+str(Z)+ ' ',end='')
         if 'X' not in line and 'Y' not in line and 'Z' not in line:
             continue
-        print()
-
         [X,Y,Z] = [float(X),float(Y),float(Z)]
         [A1,A2,A3,A4,A5,A6] = inverse_kinematics_dummy(X,Y,Z)
         write_to_KVP_memory_dummy(A1,A2,A3,A4,A5,A6)
+
+        dump_value(X,Y,Z,A1,A2,A3,A4,A5,A6)
 
 
 fin.close()
